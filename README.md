f65b4134605e47185f93421c7a663524e43ef20e
yasmani <tas185@gmail.com>
2026-07-27 11:51:38 -0500

    update readme

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ pip install python-dotenv
 
 ## For Connected RedisInsight
 redis (not localhost)
+
+### Example For test
+OPENAI_MODEL_CHAT=llama3.2:latest
+OPENAI_MODEL_EMBEDDING=nomic-embed-text:latest
+OPENAI_BASE_URL="http://localhost:11434/v1"
+OPENAI_API_KEY=dummy
+
+INDEX_NAME="embeddings-index"
+PREFIX="doc"
+DISTANCE_METRIC="COSINE"
+
+REDIS_HOST="localhost"
+REDIS_PORT="6379"
+REDIS_PASSWORD=
--- responseservice.py
+++ responseservice.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 
+from config import Settings
+
 client = OpenAI()
 
 class ResponseService():
@@ -7,7 +9,7 @@ class ResponseService():
         pass
 
     def generate_response(self, facts, user_question):
-        response = client.chat.completions.create(model="gpt-3.5-turbo",
+        response = client.chat.completions.create(model=Settings.MODEL_CHAT,
                                                   messages=[
                                                       {"role": "user", "content": 'Based on the FACTS, give an answer to the QUESTION. '+
                                                                                   f'QUESTION: {user_question}. FACTS: {facts}'}
